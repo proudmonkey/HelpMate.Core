b165ae2f581ae12172103f3fc4e9d86d4a7a9302
Vincent Maverick S Durano <vmsdurano@gmail.com>
2020-06-10 19:54:27 -0500

    [FET] Initial Commit (v1)

---
+++ HelpMate.Core/ConversionExtensions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace HelpMate.Core
+{
+    public static class TypeConverterExtensions
+    {
+        public static DateTime ToDateTime(this string value)
+            => DateTime.TryParse(value, out var result) ? result : default;
+
+        public static DateTime? ToNullableDateTime(this string value)
+            => !string.IsNullOrEmpty((value ?? "").Trim()) ? value.ToDateTime() : (DateTime?) null;
+
+        public static short ToInt16(this string value)
+            => short.TryParse(value, out var result) ? result : default;
+
+        public static int ToInt32(this string value)
+            => int.TryParse(value, out var result) ? result : default;
+
+        public static int? ToNullableInt32(this string value)
+            => !string.IsNullOrEmpty(value) ? value.ToInt32() : (int?) null;
+
+        public static long ToInt64(this string value)
+            => long.TryParse(value, out var result) ? result : default;
+
+        public static long? ToNullableInt64(this string value)
+            => !string.IsNullOrEmpty(value) ? value.ToInt64() : (long?) null;
+
+        public static bool ToBoolean(this string value)
+            => bool.TryParse(value, out var result) ? result : default;
+
+        public static float ToFloat(this string value)
+            => float.TryParse(value, out var result) ? result : default;
+
+        public static decimal ToDecimal(this string value)
+            => decimal.TryParse(value, out var result) ? result : default;
+
+        public static double ToDouble(this string value)
+           => double.TryParse(value, out var result) ? result : default;
+
+        public static string ToBase64Encode(this string value) 
+           => Convert.ToBase64String(Encoding.UTF8.GetBytes(value));
+
+        public static string ToBase64Encode(this byte[] value) 
+            => Convert.ToBase64String(value);
+
+        public static string ToBase64Decode(this string value) 
+            => Encoding.UTF8.GetString(Convert.FromBase64String(value));
+
+        public static byte[] ToByteFromBase64CharArray(this string value) 
+            => Convert.FromBase64CharArray(value.ToCharArray(), 0, value.Length);
+
+        public static byte[] ToByteArray(this string value) 
+            => Convert.FromBase64String(value);
+
+        public static string ToDateTimeFormat(this string date, string format) 
+            => date.ToDateTime().ToString(format, CultureInfo.InvariantCulture);
+
+        public static string ToCamelCase(this string value)
+        {
+            if (!string.IsNullOrEmpty(value) && value.Length > 1)
+            {
+                return char.ToLowerInvariant(value[0]) + value.Substring(1);
+            }
+
+            return value;
+        }
+
+        public static int GetYearsFromDate(this DateTime date)
+        {
+            var now = DateTime.UtcNow;
+            int years = now.Year - date.Year;
+
+            if ((date.Month > now.Month) || (date.Month == now.Month && date.Day > now.Day))
+                years--;
+
+            return years;
+        }
+
+        public static int GetYearsDifference(this DateTime date, DateTime dateToCompare)
+        {
+            int years = dateToCompare.Year - date.Year;
+
+            if ((date.Month > dateToCompare.Month) || (date.Month == dateToCompare.Month && date.Day > dateToCompare.Day))
+                years--;
+
+            return years;
+        }
+    }
+}
---
+++ HelpMate.Core/ValidationExtensions.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace HelpMate.Core
+{
+    public static class ValidationExtensions
+    {
+        public static bool IsValidEmailFormat(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) { return false; }
+
+            // only return true if there is only 1 '@' character
+            // and it is neither the first nor the last character
+            int index = value.IndexOf('@');
+
+            return
+                index > 0 &&
+                index != value.Length - 1 &&
+                index == value.LastIndexOf('@');
+        }
+
+        public static bool IsValidCreditCardNumber(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) { return false; }
+
+            value = value.Replace("-", "").Replace(" ", "");
+
+            int checksum = 0;
+            bool evenDigit = false;
+            // http://www.beachnet.com/~hstiles/cardtype.html
+            foreach (char digit in value.ToCharArray().Reverse())
+            {
+                if (!char.IsDigit(digit))
+                {
+                    return false;
+                }
+
+                int digitValue = (digit - '0') * (evenDigit ? 2 : 1);
+                evenDigit = !evenDigit;
+
+                while (digitValue > 0)
+                {
+                    checksum += digitValue % 10;
+                    digitValue /= 10;
+                }
+            }
+
+            return (checksum % 10) == 0;
+        }
+
+        public static bool IsValidJson(this string json)
+        {
+            json = json.Trim();
+            if ((json.StartsWith("{") && json.EndsWith("}")) || //For object
+                (json.StartsWith("[") && json.EndsWith("]"))) //For array
+            {
+                try
+                {
+                    using var obj = JsonDocument.Parse(json);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool IsValidDateTime(DateTime? date) 
+            => date == default ? false : true;
+
+        public static bool IsValidDateTimeString(string date) 
+            => date.ToDateTime() == default ? false : true;
+
+        public static bool IsFutureDate(this string date) 
+            => date.ToDateTime() < DateTime.Today ? false : true;
+
+        public static bool IsValidStandardDateString(this string value) 
+            => DateTime.TryParseExact(value, "yyyy-MM-dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+
+        public static bool IsNumber(this string value)
+            => Regex.IsMatch(value, @"^\d+$");
+
+        public static bool IsWholeNumber(this string value)
+            => long.TryParse(value, out _);
+
+        public static bool IsDecimalNumber(this string value)
+            => decimal.TryParse(value, out _);
+
+        public static bool IsBoolean(this string value)
+            => bool.TryParse(value, out var _);
+
+        public static bool IsHtml(this string value)
+             => Regex.IsMatch(value, @"<\s*([^ >]+)[^>]*>.*?<\s*/\s*\1\s*>");
+    }
+}
